46867ced3c4ee138e477344fd12b17fca5b412b3
Bogdan <bogdannbv@gmail.com>
2023-08-22 21:04:42 +0300

    Move slimmed-down receiver.cpp and receiver.h

--- src/receiver.cpp
+++ src/receiver.cpp
@@ -1,20 +1,18 @@
 #include "receiver.h"
-#include "wfmrx.h"
-#include <zero_file.h>
+#include "receivers/wfmrx.h"
 
 #define TARGET_QUAD_RATE 1e6
 #define WAV_FILE_GAIN 0.5
 #define AUDIO_SAMPLE_RATE 48000
 
-namespace alis::receivers {
+namespace alis {
 
     receiver::receiver(const std::string &device_id, unsigned int decimation) :
             running(false),
             recording(false),
-            center_freq(88000000), // Digi FM (RO) - closest to FM band center :D
+            center_freq(97.9 * 1e6), // Digi FM (RO) - closest to FM band center :D
             sample_rate(1.8 * 1e6),
             decimation(decimation) {
-
         if (device_id.empty()) {
             throw std::runtime_error("No device ID specified");
         }
@@ -23,6 +21,8 @@ namespace alis::receivers {
 
         // RTL-SDR Source
         src = osmosdr::source::make(get_rtl_device_string(device_id));
+        set_center_freq(center_freq);
+        set_auto_gain(true);
 
         if (decimation >= 2) {
             try {
@@ -44,26 +44,14 @@ namespace alis::receivers {
         downconverter = make_downconverter_cc(ddc_decim, 0.0, decimation_rate);
         rx = make_wfmrx((float) quad_rate, AUDIO_SAMPLE_RATE);
         rx->set_demod(wfmrx::WFMRX_DEMOD_MONO);
+        src->set_sample_rate(sample_rate);
 
         iq_swap = make_iq_swap_cc(false);
-        dc_corr = make_dc_corr_cc(decimation_rate, 1.0);
 
         wav_gain0 = gr::blocks::multiply_const_ff::make(WAV_FILE_GAIN);
         wav_gain1 = gr::blocks::multiply_const_ff::make(WAV_FILE_GAIN);
 
-        auto file_path = zero_file::get_random_tmp_file(10);
-        try {
-            wav_sink = gr::blocks::wavfile_sink::make(file_path.c_str(), 2,
-                                                      (unsigned int) AUDIO_SAMPLE_RATE,
-                                                      gr::blocks::FORMAT_WAV,
-                                                      gr::blocks::FORMAT_PCM_16
-            );
-        } catch (std::runtime_error &e) {
-            spdlog::error("Can not open {}: {}", file_path, e.what());
-        }
-
-        audio_null_sink0 = gr::blocks::null_sink::make(sizeof(float));
-        audio_null_sink1 = gr::blocks::null_sink::make(sizeof(float));
+        null_sink = gr::blocks::null_sink::make(sizeof(float));
 
         connect();
     }
@@ -75,7 +63,6 @@ namespace alis::receivers {
         last_block = src;
 
         if (decimation >= 2) {
-            spdlog::critical("HEREEE");
             top_block->connect(last_block, 0, input_decim, 0);
             last_block = input_decim;
         }
@@ -86,7 +73,7 @@ namespace alis::receivers {
 
         top_block->connect(downconverter, 0, rx, 0);
 
-        top_block->connect(rx, 0, audio_null_sink0, 0);
+        top_block->connect(rx, 0, null_sink, 0);
 
         return receiver::STATUS_OK;
     }
@@ -111,7 +98,7 @@ namespace alis::receivers {
         return receiver::STATUS_OK;
     }
 
-    receiver::status receiver::start_recording(const std::string filename) {
+    receiver::status receiver::start_recording(const std::string& filename) {
         if (recording) {
             spdlog::error("Can not start recording (already recording)");
 
@@ -124,6 +111,19 @@ namespace alis::receivers {
             return STATUS_ERROR;
         }
 
+        try {
+            wav_sink = gr::blocks::wavfile_sink::make(filename.c_str(),
+                                                      2,
+                                                      (unsigned int) AUDIO_SAMPLE_RATE,
+                                                      gr::blocks::FORMAT_WAV,
+                                                      gr::blocks::FORMAT_PCM_16
+            );
+        } catch (std::runtime_error &e) {
+            spdlog::error("Can not open {}: {}", filename, e.what());
+
+            return STATUS_ERROR;
+        }
+
         top_block->lock();
 
         top_block->connect(rx, 0, wav_gain0, 0);
@@ -132,9 +132,8 @@ namespace alis::receivers {
         top_block->connect(wav_gain1, 0, wav_sink, 1);
 
         top_block->unlock();
-        recording = true;
 
-        spdlog::critical(dot_graph(top_block));
+        recording = true;
 
         spdlog::info("Recording audio to {}", filename);
 
@@ -155,21 +154,17 @@ namespace alis::receivers {
         }
 
         top_block->lock();
+
         wav_sink->close();
         top_block->disconnect(rx, 0, wav_gain0, 0);
         top_block->disconnect(rx, 1, wav_gain1, 0);
         top_block->disconnect(wav_gain0, 0, wav_sink, 0);
         top_block->disconnect(wav_gain1, 0, wav_sink, 1);
 
-        // Temporary workaround for https://github.com/gnuradio/gnuradio/issues/5436
-        top_block->disconnect(downconverter, 0, rx, 0);
-        top_block->connect(downconverter, 0, rx, 0);
-        // End temporary workaronud
-
         top_block->unlock();
-        wav_gain0.reset();
-        wav_gain1.reset();
+
         wav_sink.reset();
+
         recording = false;
 
         spdlog::info("Audio recorder stopped");
@@ -187,39 +182,6 @@ namespace alis::receivers {
         src->set_center_freq(center_freq);
 
         return receiver::STATUS_ERROR;
-    }
-
-    double receiver::set_sample_rate(double rate) {
-        double current_rate;
-        bool rate_has_changed;
-
-        current_rate = src->get_sample_rate();
-
-        rate_has_changed = !(rate == current_rate ||
-                             std::abs(rate - current_rate) < std::abs(std::min(rate, current_rate))
-                                                             * std::numeric_limits<double>::epsilon());
-
-        top_block->lock();
-
-        try {
-            sample_rate = src->set_sample_rate(rate);
-        }
-        catch (std::runtime_error &e) {
-            sample_rate = 0;
-        }
-
-        if (sample_rate == 0) {
-            // This can be the case when no device is attached and gr-osmosdr
-            // puts in a null_source with rate 100 ksps or if the rate has not
-            // changed
-            if (rate_has_changed) {
-                std::cerr << std::endl;
-                std::cerr << "Failed to set RX input rate to " << rate << std::endl;
-                std::cerr << "Your device may not be working properly." << std::endl;
-                std::cerr << std::endl;
-            }
-            sample_rate = rate;
-        }
     }
 
 } // receivers
--- src/receiver.h
+++ src/receiver.h
@@ -14,19 +14,13 @@
 #include "dsp/downconverter.h"
 #include "dsp/filter/fir_decim.h"
 #include "dsp/rx_noise_blanker_cc.h"
-#include "dsp/rx_filter.h"
-#include "dsp/rx_meter.h"
-#include "dsp/rx_agc_xx.h"
 #include "dsp/rx_demod_fm.h"
-#include "dsp/rx_demod_am.h"
-#include "dsp/rx_fft.h"
-#include "dsp/sniffer_f.h"
 #include "dsp/resampler_xx.h"
 #include "receivers/receiver_base.h"
 
 #include "zero_file.h"
 
-namespace alis::receivers {
+namespace alis {
 
     class receiver {
     public:
@@ -35,33 +29,19 @@ namespace alis::receivers {
             STATUS_ERROR = 1,
         };
 
-        enum filter_shape {
-            FILTER_SHAPE_SOFT = 0,   /*!< Soft: Transition band is TBD of width. */
-            FILTER_SHAPE_NORMAL = 1, /*!< Normal: Transition band is TBD of width. */
-            FILTER_SHAPE_SHARP = 2   /*!< Sharp: Transition band is TBD of width. */
-        };
-
         explicit receiver(const std::string &device_id = "", unsigned int decimation = 1);
-
-//        ~receiver();
 
         void start();
 
         void stop();
 
-        double set_sample_rate(double rate);
-
         status set_center_freq(double freq_hz);
 
         status set_auto_gain(bool automatic);
 
-//        status set_demod(rx_demod demod, bool force = false);
+        status start_recording(const std::string& filename);
 
-        status set_af_gain(float gain_db);
-
-        status start_recording(const std::string filename);
-
-        bool is_recording(void) const { return recording; }
+        bool is_recording() const { return recording; }
 
         status stop_recording();
 
@@ -80,11 +60,7 @@ namespace alis::receivers {
         fir_decim_cc_sptr input_decim;
         receiver_base_cf_sptr rx;
 
-        dc_corr_cc_sptr dc_corr;
         iq_swap_cc_sptr iq_swap;
-
-        rx_fft_c_sptr iq_fft;
-        rx_fft_f_sptr audio_fft;
 
         downconverter_cc_sptr downconverter;
 
@@ -92,11 +68,7 @@ namespace alis::receivers {
         gr::blocks::multiply_const_ff::sptr wav_gain1; /*!< WAV file gain block. */
 
         gr::blocks::wavfile_sink::sptr      wav_sink;
-        gr::blocks::null_sink::sptr         audio_null_sink0;
-        gr::blocks::null_sink::sptr         audio_null_sink1;
-
-        sniffer_f_sptr    sniffer;
-        resampler_ff_sptr sniffer_rr;
+        gr::blocks::null_sink::sptr         null_sink;
 
     private:
         static std::string get_rtl_device_string(const std::string &device_id);
